3281b44db88cb38d7a05b764962e7ffe830a2978
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-05-04 10:51:16 +0530

    apprender

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import Header from "./components/header/Header"
-
+import Header from "./components/header/Header";
 import "./App.css";
 import Middle from "./components/middle_section/Middle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Middle />
+    </>
   );
 }
 
