b714570cf8dc9184a765947435f1712370d11f6e
rhutikthakare <thakarerhutik@gmail.com>
2023-05-04 15:12:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,16 @@ import logo from './logo.svg';
 import Textfield from './components/textfield/Textfield';
 import RightSession from './components/textfield/RightSession';
 import Combine from './components/textfield/Combine';
-
+import Header from './components/header/Header';
+import Middle from './components/middle_section/Middle'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Middle/>
+    <Combine/>
+
     </div>
   );
 }
