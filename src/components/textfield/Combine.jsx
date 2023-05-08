bf78089619a178093e6914ec3ce349168ad5c53a
subhadippaul2311 <subhadippaul2001@gmail.com>
2023-05-08 10:00:08 +0530

    name change

--- src/components/textfield/Combine.jsx
+++ src/components/textfield/Combine.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import RightSession from "./RightSession";
+import LeftSession from "./LeftSession";
 import Textfield from "./Textfield";
 import styles from "./Combine.module.css";
 
 function Combine() {
   return (
     <div className={styles.main}>
-      <RightSession />
+      <LeftSession />
       <Textfield />
     </div>
   );
